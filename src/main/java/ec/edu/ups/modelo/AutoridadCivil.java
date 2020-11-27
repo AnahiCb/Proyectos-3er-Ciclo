26bf0d567ad275bb855a0b723e3ac707f6a7b379
AnahiCb <ecabrerab1@est.ups.edu.ec>
2020-11-27 11:17:10 -0500

    Prueba1 modelo

---
+++ src/main/java/ec/edu/ups/modelo/AutoridadCivil.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.modelo;
+
+/**
+ *
+ * @author Anahi
+ */
+public class AutoridadCivil {
+    private String cargo;
+    private String correo;
+    private String contrasenia;
+}
